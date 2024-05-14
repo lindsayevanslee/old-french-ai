8759c16da4c02ee5c89dd4400f9e66861c226723
Lindsay Lee <yasdnil922@comcast.net>
2024-05-13 21:26:22 -0500

    Add more extension types to delete

--- delete-non-images.py
+++ delete-non-images.py
@@ -3,6 +3,8 @@ import os
 #Delete files with specific extensions
 def delete_files_with_extensions(directory, extensions):
     for root, dirs, files in os.walk(directory):
+        #print(f"root: {root}")
+        #print(f"dirs: {dirs}")
         for file in files:
             if file.endswith(tuple(extensions)):
                 file_path = os.path.join(root, file)
@@ -11,5 +13,5 @@ def delete_files_with_extensions(directory, extensions):
 
 # Set directory and extensions
 directory = "data/ME MSS Images/heic"
-extensions = ["DS_Stores", ".MOV", ".mov", ".docx"]  # Add your desired file extensions here
+extensions = ["DS_Store", ".MOV", ".mov", ".docx", ".pdf"]  # Add your desired file extensions here
 delete_files_with_extensions(directory, extensions)
