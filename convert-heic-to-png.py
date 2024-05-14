2f9bef065d6658757a51c7eb356edabb35193fba
Lindsay Lee <yasdnil922@comcast.net>
2024-05-13 21:04:58 -0500

    Adapt conversion step to skip files already converted

--- convert-heic-to-png.py
+++ convert-heic-to-png.py
@@ -31,67 +31,102 @@ register_heif_opener()
 
 # Loop through all files recursively in the input directory
 for root, dirs, files in os.walk(input_dir):
-    #print(f"root: {root}")
-    #print(f"dirs: {dirs}")
+    print(f"root: {root}")
+    print(f"dirs: {dirs}")
     #print(f"files: {files}")
     for filename in files:
         if filename.endswith('.HEIC') or filename.endswith('.heic'):
-            # Open the HEIC file
+            # Create the output directory if it doesn't exist
+            new_root = root.replace('heic', 'png')
+            os.makedirs(new_root, exist_ok=True)
+            
+            # Get heic path
             heic_path = os.path.join(root, filename)
-            print(heic_path)
+            #print(heic_path)
 
+            # Get the PNG filename and path
+            png_filename = os.path.splitext(filename)[0] + '.png'
+            #print(png_filename)
+            png_path = os.path.join(new_root, png_filename)
+            #print(png_path)
+
+            # If the PNG file already exists, skip the conversion
+            if os.path.exists(png_path):
+                print(f"File already exists: {png_path}")
+                continue
+            else:
+                print(f"Converting: {heic_path} to {png_path}")
+
+            # Open the HEIC file
             heic_image = Image.open(heic_path)
 
             # Convert the image to PNG format
             png_image = heic_image.convert('RGB')
-
+            
             # Save the PNG image in the output directory
-            png_filename = os.path.splitext(filename)[0] + '.png'
-            print(png_filename)
-
-            # Create the output directory if it doesn't exist
-            new_root = root.replace('heic', 'png')
-            os.makedirs(new_root, exist_ok=True)
-
-            png_path = os.path.join(root.replace('heic', 'png'), png_filename)
-            print(png_path)
             png_image.save(png_path, 'PNG')
 
             # Close the image
             heic_image.close()
+
+
         elif filename.endswith('.jpeg') or filename.endswith('.jpg'):
-            # Open the JPEG file
-            jpeg_path = os.path.join(root, filename)
-            print(jpeg_path)
-
-            jpeg_image = Image.open(jpeg_path)
-
-            # Save the JPEG image in the output directory
-            jpeg_filename = os.path.splitext(filename)[0] + '.png'
-            print(jpeg_filename)
-
+            
             # Create the output directory if it doesn't exist
             new_root = root.replace('heic', 'png')
             os.makedirs(new_root, exist_ok=True)
+            
+            # Get the JPEG path
+            jpeg_path = os.path.join(root, filename)
+            #print(jpeg_path)
 
-            jpeg_path = os.path.join(new_root, jpeg_filename)
-            print(jpeg_path)
-            jpeg_image.save(jpeg_path, 'PNG')
+            # Get the PNG filename and path
+            png_filename = os.path.splitext(filename)[0] + '.png'
+            #print(png_filename)
+            png_path = os.path.join(new_root, png_filename)
+            #print(png_path)
+
+            # If the PNG file already exists, skip the conversion
+            if os.path.exists(png_path):
+                print(f"File already exists: {png_path}")
+                continue
+            else:
+                print(f"Converting: {jpeg_path} to {png_path}")
+
+            # Open the JPEG file
+            jpeg_image = Image.open(jpeg_path)
+
+            # Save the JPEG image in the output directory
+            jpeg_image.save(png_path, 'PNG')
 
             # Close the image
             jpeg_image.close()
 
         elif filename.endswith('.png'):
+
+            # Create the output directory if it doesn't exist
+            new_root = root.replace('heic', 'png')
+            os.makedirs(new_root, exist_ok=True)
+
             #Copy the PNG file to the output directory
             png_path_orig = os.path.join(root, filename)
-            print(png_path_orig)
+            #print(png_path_orig)
+
+            # Get the output PNG filename and path
+            png_filename = os.path.splitext(filename)[0] + '.png'
+            #print(png_filename)
+            png_path = os.path.join(root.replace('heic', 'png'), png_filename)
+            #print(png_path)
+
+
+            # If the PNG file already exists, skip the conversion
+            if os.path.exists(png_path):
+                print(f"File already exists: {png_path}")
+                continue
+            else:
+                print(f"Copying: {png_path_orig} to {png_path}")
 
             #Copy the PNG image in the output directory
-            png_filename = os.path.splitext(filename)[0] + '.png'
-            print(png_filename)
-
-            png_path = os.path.join(root.replace('heic', 'png'), png_filename)
-            print(png_path)
             os.system(f'cp "{png_path_orig}" "{png_path}"')
 
 
