a056b9057f13500999b85e4bec36667d19d54223
Lindsay Lee <yasdnil922@comcast.net>
2024-05-14 20:53:45 -0500

    Add correct-skew.py to run-all.py

--- run_all.py
+++ run_all.py
@@ -3,7 +3,8 @@ import subprocess
 # List of scripts to run
 preprocessing_scripts = ["delete-non-images.py", 
            "convert-heic-to-png.py", 
-           "checks.py"]
+           "checks.py",
+           "correct-skew.py"]
 
 for script in preprocessing_scripts:
     subprocess.run(["python", script])
