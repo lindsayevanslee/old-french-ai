cacd8fb74c4a17a090e1bfc68c2331325f7ba04b
Lindsay Lee <yasdnil922@comcast.net>
2024-07-28 16:47:48 -0500

    Update resizing script to use the new input path

--- reduce-size.py
+++ reduce-size.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 
 #Set input directory
-input_dir = 'data/ME MSS Images/png/T-F2'
+input_dir = 'data/ME MSS Images/test images folder structure/output/O-E'
 
 mb_limit = 10
 bytes_to_mb = 1000 * 1000 #Finder uses base 10, but many programming languages use base 2
@@ -15,10 +15,14 @@ def reduce_image_size(input_dir):
                 #file_size = os.path.getsize(file_path)
                 file_size = os.stat(file_path).st_size
 
-                #create path of output binary image
-                new_root = root.replace('png', 'png_resized')
-                os.makedirs(new_root, exist_ok=True)
-                output_path = os.path.join(new_root, file)
+
+                #create path of output image
+                #new_root = root.replace('png', 'png_resized')
+                #os.makedirs(new_root, exist_ok=True)
+                #output_path = os.path.join(new_root, file)
+                output_path = os.path.join(root, "01_resized.png")
+                print(output_path)
+                
 
                 print(f"File: {file}, Size: {round(file_size/bytes_to_mb, 1)} MB.")
                 
@@ -35,6 +39,8 @@ def reduce_image_size(input_dir):
 
                     #Copy the PNG image in the output directory
                     os.system(f'cp "{file_path}" "{output_path}"')
+
+                
 
 # Call the function
 reduce_image_size(input_dir)
