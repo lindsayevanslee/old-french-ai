18f09c54e6468893b0479ec51b6d50774d5a65b4
Lindsay Lee <yasdnil922@comcast.net>
2024-09-02 20:38:42 -0500

    Add remove-background.py to run_all.py

--- run_all.py
+++ run_all.py
@@ -5,6 +5,7 @@ preprocessing_scripts = ["delete-non-images.py",
            "convert-heic-to-png.py", 
            "checks.py",
            "reduce-size.py",
+           "remove-background.py",
            "correct-skew.py"]
 
 for script in preprocessing_scripts:
